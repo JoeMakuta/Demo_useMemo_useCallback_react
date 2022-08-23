07c80f63b517c0e69f79c6b4b314d06d8349f36e
Josue Makuta <makutajosue@gmail.com>
2022-08-24 00:24:19 +0200

    Feat : Use memo added to optimize the components rendering ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+
+import { useCallback, useMemo, useState } from 'react'
+
+
+function changeBackground(color) {
+  for (let i = 0; i < 1000000000; i++) { }
+  if (color) return (false)
+  else return (true)
+}
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+  const [number, setNumber] = useState(0)
+  const [darkMode, setDarkMode] = useState(false)
+
+  const colorStyle = {
+    backgroundColor: darkMode ? 'black' : 'white',
+    color: darkMode ? 'white' : 'black'
+  }
+
+  function decrement() {
+    console.log('Decrement');
+    setNumber(number - 1)
+  }
+
+  function increment() {
+    console.log('Increment');
+    setNumber(number + 1)
+  }
+
+  const myColor = useMemo(() => changeBackground(darkMode), [darkMode])
+
+  return (<div style={colorStyle}>
+    <h1>
+      React Hooks :<br />
+      <span>useCallback</span> and <span>useMemo</span>
+    </h1>
+
+    <button onClick={() => { decrement() }}>-</button>
+    <strong>{number}</strong>
+    <button onClick={() => { increment() }}>+</button>
+
+    <div>
+      <button onClick={() => (setDarkMode(myColor))}>Change background </button>
     </div>
-  )
+
+  </div>)
 }
 
 export default App
